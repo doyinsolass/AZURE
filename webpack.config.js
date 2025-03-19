b0c7bc5f7561e6b8f964827e212082d34d9a2e4a
doyinsola <153112659+doyinsolass@users.noreply.github.com>
2025-03-19 16:13:46 +0000

    h

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // This should point to the correct entry file
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify') // Add this line
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
   },
 };
